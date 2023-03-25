12560a5d3f4493722ee7c3be4077158e4f64d764
Basu <bmugganavar@falabella.cl>
2023-03-25 19:14:05 +0530

    initial UI added

--- WalmartTest/ViewController.swift
+++ WalmartTest/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
